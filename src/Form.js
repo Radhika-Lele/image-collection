0512e31bbf8864431fbd8f2d4108c79205d08d75
Radhika-Lele <radhika.khaparde@gmail.com>
2023-07-12 11:46:52 -0400

    add pop up image and make responsive design

--- src/Form.js
+++ src/Form.js
@@ -13,7 +13,7 @@ function Form({getPhotos}){
     <div className='wrapper'>
     <form className="formSelect" onSubmit={(event) => getPhotos(event, userChoice)}>
         <label htmlFor="photoOrientation">Show me photos that are:</label>
-        <select id="photoOrientation" name="photoOrientation" onChange={handleUserChoice} value={userChoice}>
+        <select id="photoOrientation" name="photoOrientation" onChange={handleUserChoice} value={userChoice} required>
         <option value="" disabled>Pick one:</option>
         <option value="square">square</option>
         <option value="landscape">landscape</option>
